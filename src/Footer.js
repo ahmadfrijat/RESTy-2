8dc6c439c911fce2b1093f65237c140247154162
ahmadfrijat <ahmadalfrijat7@gmail.com>
2021-05-04 00:34:32 +0300

    lab27

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.scss';
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>CopyRight@Ahmad-Frijat2021</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Form.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
+import Form from './Form.js';
+// import Results from './results.js';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('<Form />', ()=>{
+  it('exists at the start of the application',()=>{
+    let app = shallow(<Form />);
+    expect(app.find('button').exists()).toBeTruthy();
+    expect(app.find('input').exists()).toBeTruthy();
+    expect(app.find('form').exists()).toBeTruthy();
+
+  });
+  xit('can correctly GET Method',()=>{
+    let app = mount(<Form />);
+    let button = app.find('button');
+    button.simulate('click');
+    expect(app.state('Headers')).toBe(undefined);
+    expect(app.state('Response')).toBe(undefined);
+  });
+  it('renders correctly', () => {
+    const tree = renderer.create(<Form />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
